da258aa947f81032ecc72cbfa46fc266314fabf8
mcs58A <safety.mcs58@gmail.com>
2025-01-04 01:37:59 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "health-form-cache-v2";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
